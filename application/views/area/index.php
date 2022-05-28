7b21e6c23bfcc796a1c3c1384cca7e0004118257
Carlos Aguirre <carlosfelipeaguirre96@gmail.com>
2022-05-28 18:50:26 -0500

    mejorado el responsive

--- application/views/area/index.php
+++ application/views/area/index.php
@@ -8,19 +8,19 @@ $this->load->view("layout/header", $data);
 <div class="w3-card w3-container" >
   <h3 class="w3-margin">Crear o Editar Áreas</h3>
   <div class="w3-row-padding" style="margin-bottom: 3em;">
-    <div class="w3-col s2">
+    <div class="w3-col m2 s6">
       <p>
         Código
         <input class="w3-input" id="codigo"/>
       </p>
     </div>
-    <div class="w3-col s2">
+    <div class="w3-col m2 s6">
       <p>
         Nombre
         <input class="w3-input" id="nombre"/>
       </p>
     </div>
-    <div class="w3-col s2">
+    <div class="w3-col m2 s12">
       <button class="w3-btn w3-teal" style="margin-top: 2.2em;" @click="create">Aceptar</button>
     </div>
   </div>
--- application/views/user/home.php
+++ application/views/user/home.php
@@ -8,25 +8,25 @@ $this->load->view("layout/header", $data);
 <div class="w3-card w3-container" >
   <h3 class="w3-margin">Crear o Editar Materias</h3>
   <div class="w3-row-padding" style="margin-bottom: 4em;">
-    <div class="w3-col s2">
+    <div class="w3-col m2 s6">
       <p>
         Código
         <input type="number" class="w3-input" v-model="codigo" />
       </p>
     </div>
-    <div class="w3-col s3">
+    <div class="w3-col m3 s6">
       <p>
         Nombre
         <input type="text" class="w3-input" v-model="nombre" />
       </p>
     </div>
-    <div class="w3-col s2">
+    <div class="w3-col m2 s4" >
       <p>
         Horas por semana
         <input type="number" class="w3-input" v-model="hrs_semana" />
       </p>
     </div>
-    <div class="w3-col s3">
+    <div class="w3-col m3 s4">
       <p>
         Área
         <select class="w3-input" v-model="area">
@@ -37,7 +37,7 @@ $this->load->view("layout/header", $data);
         </select>
       </p>
     </div>
-    <div class="w3-col s2">
+    <div class="w3-col m2 s4">
       <button class="w3-btn w3-teal" style="margin-top: 2.4em;" @click="create">Aceptar</button>
     </div>
   </div>
